85259345a937858f92b19eb5c9a0248f4e73fe99
John Travers <johnk.travers@gmail.com>
2019-08-27 22:43:31 -0600

    Add world cup class and test

---
+++ lib/world_cup.rb
@@ -0,0 +1,26 @@
+class WorldCup
+  attr_reader :year, :teams
+
+  def initialize(year, teams)
+    @year = year
+    @teams = teams
+  end
+
+  def active_players_by_position(position)
+    players = []
+
+    @teams.each do |team|
+      if !team.eliminated?
+        team.players.each do |player|
+          if player.position == position
+            players.push(player)
+          end
+        end
+      end
+    end
+    
+    players
+  end
+
+
+end
